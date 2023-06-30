02f55c69b6630c5977a39e61f1673381ffe68ca9
mabi-max <florakeyen@gmail.com>
2023-06-30 01:22:31 +0100

    navigation bar competed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,81 @@
-import React from "react";
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import React, { useState } from 'react';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 
-const Navbar = () => {
-    return (
-        <div className="digitech__navbar">
-            <div className="digitech__navbar-links">
-                <div className="digitech__navbar-links-logo">
-                   <img src={logo} alt="logo"/>
-                </div>
-                <div className="digitech__navbar-links-container">
-                    <p><a href="#home">Home</a></p>
-                    <p><a href="#wdgt">What is DigiTech</a></p>
-                    <p><a href="#possibility">Open AI</a></p>
-                    <p><a href="#features">Case studies</a></p>
-                    <p><a href="#Library">Library</a></p>
-                </div>
+
+
+function Navigation() {
+  return (
+    <nav className="navbar navbar-dark sticky-top" expand="lg">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <div>
+            <div className="digitech__navbar-links_logo">
+              DigiTech
             </div>
-            <div className="digitech__navbar-sign">
-                <p>Sign in</p>
-                <button type="button">Sign up</button>
-            </div>
+          </div>
+        </a>
+
+        <div className="d-flex">
+          <p className="text-white m-1 d-none d-lg-block" type="button">
+            Sign In
+          </p>
+          <button className="btn btn-danger me-2 d-none d-lg-block" type="button">
+            Sign Up
+          </button>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
         </div>
-    )
+
+        <div className="collapse navbar-collapse custom-menu" id="collapsibleNavbar">
+         
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                What is Digitech?
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                Open AI
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="logout.php">
+                Case studies
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="logout.php">
+                Library
+              </a>
+            </li>
+            <li>
+              <div className='d-flex'>
+            <p className="text-white m-2 d-lg-none" type="button">
+            Sign In 
+          </p>
+          <button className="btn btn-danger me-2 d-lg-none" type="button">
+            Sign Up
+          </button>
+           </div>
+            </li>
+          </ul>
+        </div>
+
+        
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar;
+
+
+export default Navigation;
