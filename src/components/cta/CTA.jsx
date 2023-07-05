91ecf728b7d9337286d15b02664faa58fb68c7a5
mabi-max <florakeyen@gmail.com>
2023-07-05 14:15:23 +0100

    cta component done

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,14 @@ import './cta.css';
 
 const CTA = () => {
     return (
-        <div>
-            CTA
+        <div className="digitech__cta">
+            <div className="digitech__cta-content">
+                <p>Request Early Access to Get Started</p>
+                <h3>Register today & start exploring the endless possibilities</h3>
+            </div>
+            <div className="digitech__cta-button">
+                <button type="button">Get Started</button>
+            </div>
         </div>
     )
 }
