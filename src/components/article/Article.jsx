f43d48f14f191e3b331878e7ef4cfcf719e71376
mabi-max <florakeyen@gmail.com>
2023-07-05 15:31:02 +0100

    article section done

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import './article.css';
 
-const Article = () => {
+const Article = ({ imageUrl, date, title }) => {
     return (
-        <div>
-            Article
+        <div className="digitech__blog-container_article">
+            <div className="digitech__blog-container_article-image">
+                <img src={imageUrl} alt="blog" />
+            </div>
+            <div className="digitech__blog-container_article-content">
+                <div>
+                    <p>{date}</p>
+                    <h3>{title}</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
         </div>
     )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
 const Blog = () => {
     return (
-        <div>
-            Blog
+        <div className="digitech__blog section__padding" id="blog">
+            <div className="digitech__blog-heading">
+                <h1 className="gradient__text">A lot is happening. We are blogging about it.</h1>
+            </div>
+            <div className="digitech__blog-container">
+                <div className="digitech__blog-container_groupA">
+                 <Article imageUrl={blog01} date="July 5, 2023" title="Open AI is the future. Let us explore!"/>
+                </div>
+                <div className="digitech__blog-container_groupB">
+                <Article imageUrl={blog02} date="July 5, 2023" title="Open AI is the future. Let us explore!"/>
+                <Article imageUrl={blog03} date="July 5, 2023" title="Open AI is the future. Let us explore!"/>
+                <Article imageUrl={blog04} date="July 5, 2023" title="Open AI is the future. Let us explore!"/>
+                <Article imageUrl={blog05}  date="July 5, 2023" title="Open AI is the future. Let us explore!"/>
+                </div>
+            </div>
         </div>
     )
 }
